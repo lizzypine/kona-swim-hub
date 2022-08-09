3a0001f58585e2ebe9a4502bea643000578676dd
Lizzy <elizabethpine4@gmail.com>
2022-08-08 14:41:09 -1000

    Wrote tests for User model, and homepage.

--- lessons/tests.py
+++ lessons/tests.py
@@ -1,12 +1,57 @@
-from django.test import SimpleTestCase
+# from django.test import SimpleTestCase
+from django.test import TestCase
 from django.urls import reverse
 
+from .models import User
+
+
 # Create your tests here.
-class HomepageTests(SimpleTestCase):
+# class HomepageTests(SimpleTestCase):
+#     def test_url_exists_at_correct_location(self):
+#         response = self.client.get("/")
+#         self.assertEqual(response.status_code,200)
+    
+#     def test_url_available_by_name(self):
+#         response = self.client.get(reverse("home"))
+#         self.assertEqual(response.status_code, 200)
+
+#     def test_template_name_correct(self):
+#         response = self.client.get(reverse("home"))
+#         self.assertTemplateUsed(response, "home.html")
+
+#     def test_template_content(self):
+#         response = self.client.get(reverse("home"))
+#         self.assertContains(response, "<h1>Homepage</h1")
+
+# class AboutpageTests(SimpleTestCase):
+#     def test_url_exists_at_correct_location(self):
+#         response = self.client.get("/about/")
+#         self.assertEqual(response.status_code, 200)
+
+#     def test_url_available_by_name(self):
+#         response = self.client.get(reverse("about"))
+#         self.assertEqual(response.status_code, 200)
+    
+#     def test_template_name_correct(self):
+#         response = self.client.get(reverse("about"))
+#         self.assertTemplateUsed(response, "about.html")
+
+#     def test_template_content(self):
+#         response = self.client.get(reverse("about"))
+#         self.assertContains(response, "<h1>About page</h1")
+
+class UserTests(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = User.objects.create(email="This is a test!")
+
+    def test_model_content(self):
+        self.assertEqual(self.user.email, "This is a test!")
+
     def test_url_exists_at_correct_location(self):
         response = self.client.get("/")
         self.assertEqual(response.status_code,200)
-    
+
     def test_url_available_by_name(self):
         response = self.client.get(reverse("home"))
         self.assertEqual(response.status_code, 200)
@@ -17,21 +62,4 @@ class HomepageTests(SimpleTestCase):
 
     def test_template_content(self):
         response = self.client.get(reverse("home"))
-        self.assertContains(response, "<h1>Homepage</h1")
-
-class AboutpageTests(SimpleTestCase):
-    def test_url_exists_at_correct_location(self):
-        response = self.client.get("/about/")
-        self.assertEqual(response.status_code, 200)
-
-    def test_url_available_by_name(self):
-        response = self.client.get(reverse("about"))
-        self.assertEqual(response.status_code, 200)
-    
-    def test_template_name_correct(self):
-        response = self.client.get(reverse("about"))
-        self.assertTemplateUsed(response, "about.html")
-
-    def test_template_content(self):
-        response = self.client.get(reverse("about"))
-        self.assertContains(response, "<h1>About page</h1")
+        self.assertContains(response, "<h1>Homepage</h1>")
