c7d6975aa6f715fddf72ced2d46cd1463a38d99a
Lizzy <elizabethpine4@gmail.com>
2022-11-02 13:28:59 -1000

    Security updates before making git repo public

--- django_project/settings.py
+++ django_project/settings.py
@@ -23,12 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#fu)9%_l)ai(2mjd1r*dfhbooi4r(kubh19y%u0!f*34*%7d2*'
+SECRET_KEY = env.str('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env.bool('DEBUG', default=False)
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
