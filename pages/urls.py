662b4298f1beb7967a136f363b5f5973215a9669
Lizzy <elizabethpine4@gmail.com>
2023-01-02 08:16:24 -1000

    Fixed link to signup page

--- pages/urls.py
+++ pages/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from pages.views import HomePageView, AboutPageView
+from accounts import views
 
 urlpatterns = [
     path("about/", AboutPageView.as_view(), name="about"),
+    path("signup/", views.user_register, name="signup"),
     path("", HomePageView.as_view(), name="home"),
 ]
