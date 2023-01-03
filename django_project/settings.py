658aadc365fe94832b3bfb52b3defaf4cc424a3d
Lizzy <elizabethpine4@gmail.com>
2023-01-02 21:03:48 -1000

    Updated login url

--- django_project/settings.py
+++ django_project/settings.py
@@ -116,8 +116,8 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-LOGIN_REDIRECT_URL = 'home'
-LOGOUT_REDIRECT_URL = 'home'
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
 
 TIME_INPUT_FORMATS = ['%I:%M %p',]
 
