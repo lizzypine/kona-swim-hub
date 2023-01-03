0d45578db237b8a27e9e98b6f00e2a6c5fd981b6
Lizzy <elizabethpine4@gmail.com>
2023-01-03 07:26:41 -1000

    Registration page updates

--- django_project/settings.py
+++ django_project/settings.py
@@ -127,11 +127,20 @@ AUTH_PROFILE_MODULE = 'accounts.UserProfile'
 
 FORM_RENDERER = 'django.forms.renderers.TemplatesSetting'
 
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_HOST = 'smtp.sendgrid.net'
+# EMAIL_PORT = 587
+# EMAIL_USE_TLS = True
+# EMAIL_HOST_USER = 'apikey'
+# EMAIL_HOST_PASSWORD = config('SENDGRID_API_KEY')
+# CONTACT_EMAIL = config('CONTACT_EMAIL')
+# DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL')
+
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL')
 EMAIL_HOST = 'smtp.sendgrid.net'
-EMAIL_HOST_USER = 'apikey'
-EMAIL_HOST_PASSWORD = config('SENDGRID_API_KEY')
-CONTACT_EMAIL = config('CONTACT_EMAIL')
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'apikey'
+EMAIL_HOST_PASSWORD = os.getenv('SENDGRID_API_KEY')
+CONTACT_EMAIL = os.getenv('CONTACT_EMAIL')
+DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL')
