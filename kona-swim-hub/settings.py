5919438f4a6f3a275e8debb807202576dc35af36
Lizzy <elizabethpine4@gmail.com>
2023-06-09 22:37:13 -1000

    Updates to static file storage

--- kona-swim-hub/settings.py
+++ kona-swim-hub/settings.py
@@ -124,8 +124,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, '...', 'www', 'files')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, '...', 'www', 'static')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 if 'AWS_STORAGE_BUCKET_NAME' in os.environ:
