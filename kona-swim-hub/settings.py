0aae1c571d4ed2a0264b3b44550d35240397dd7d
Lizzy <elizabethpine4@gmail.com>
2023-06-13 11:36:38 -1000

    Update handling of static files

--- kona-swim-hub/settings.py
+++ kona-swim-hub/settings.py
@@ -145,12 +145,13 @@ STATIC_ROOT = os.path.join(BASE_DIR, '..', 'www', 'static')
 # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 USE_S3 = os.getenv('USE_S3') == 'TRUE'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # if USE_S3:
 #     # aws settings
 #     # AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
 #     AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
-#     # SECRET_KEY = os.environ['SECRET_KEY']
+    # SECRET_KEY = os.environ['SECRET_KEY']
 #     # SECRET_KEY = os.getenv['SECRET_KEY']
 #     # AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
 #     # AWS_S3_BUCKET_NAME_STATIC = AWS_STORAGE_BUCKET_NAME
