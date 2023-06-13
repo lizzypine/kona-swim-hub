4c70d502fe03f1cf8c8a5c40f410ecbf2c6a49f7
Lizzy <elizabethpine4@gmail.com>
2023-06-13 09:47:17 -1000

    Update settings for static file storage

--- kona-swim-hub/settings.py
+++ kona-swim-hub/settings.py
@@ -124,8 +124,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
+# STATIC_URL = '/static/'
+# STATIC_ROOT = 'static'
 STATIC_URL = '/static/'
-STATIC_ROOT = 'static'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_URL = '/staticfiles/'
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 USE_S3 = os.getenv('USE_S3') == 'TRUE'
 
