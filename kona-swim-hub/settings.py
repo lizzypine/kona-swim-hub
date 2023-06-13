12a94ed4d59eb2ebfc0f7635a6b9da7347d746b3
Lizzy <elizabethpine4@gmail.com>
2023-06-13 12:08:34 -1000

    Update handling of static files

--- kona-swim-hub/settings.py
+++ kona-swim-hub/settings.py
@@ -131,13 +131,18 @@ USE_TZ = True
 # STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 # STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
-STATIC_ROOT = os.path.join(BASE_DIR, '..', 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, '..', 'static')
 
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
+# STATIC_URL = '/static/'
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static'),
+# ]
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
+
+STATIC_URL = "/static/"
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+# STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_ROOT = os.path.join(BASE_DIR, '..', 'www', 'static')
 
 # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # STATIC_ROOT = 'static'
