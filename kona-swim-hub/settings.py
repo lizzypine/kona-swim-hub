fb3a719626d0b6d9c05be776a26ac11c41ccf829
Lizzy <elizabethpine4@gmail.com>
2023-06-13 10:28:25 -1000

    Update settings for collectstatic

--- kona-swim-hub/settings.py
+++ kona-swim-hub/settings.py
@@ -132,7 +132,9 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = 'static'
+
 # STATIC_URL = '/staticfiles/'
 # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
