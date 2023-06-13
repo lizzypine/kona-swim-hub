ff4b2949066a1723a12de8e300d3104e7f65f5ec
Lizzy <elizabethpine4@gmail.com>
2023-06-12 14:13:29 -1000

    Update static file collection for eb

--- kona-swim-hub/settings.py
+++ kona-swim-hub/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'bootstrap_datepicker_plus',
     'django.forms',
     'crispy_forms',
-    'django_s3_storage'
+    'storages'
 ]
 
 MIDDLEWARE = [
@@ -128,10 +128,10 @@ USE_S3 = os.getenv('USE_S3') == 'TRUE'
 
 if USE_S3:
     # aws settings
-    AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
+    # AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
-    AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
-    AWS_S3_BUCKET_NAME_STATIC = AWS_STORAGE_BUCKET_NAME
+    # AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
+    # AWS_S3_BUCKET_NAME_STATIC = AWS_STORAGE_BUCKET_NAME
     AWS_DEFAULT_ACL = 'public-read'
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
     AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
@@ -139,10 +139,20 @@ if USE_S3:
     STATIC_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
     AWS_LOCATION = 'static'
     # STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_LOCATION}/'
-    STATICFILES_STORAGE = 'django_s3_storage.storage.StaticS3Storage'
+    # STATICFILES_STORAGE = 'django_s3_storage.storage.StaticS3Storage'
 else:
     STATIC_URL = '/staticfiles/'
     # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+if 'AWS_STORAGE_BUCKET_NAME' in os.environ:
+    STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+    AWS_STORAGE_BUCKET_NAME = os.environ['AWS_STORAGE_BUCKET_NAME']
+    AWS_S3_REGION_NAME = os.environ['AWS_S3_REGION_NAME']
+
+    AWS_S3_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
+    AWS_S3_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
 
 # STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'))
 
