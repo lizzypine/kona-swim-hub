95f7b542c2da376428bdf91437906f4128405541
Lizzy <elizabethpine4@gmail.com>
2023-06-13 13:11:24 -1000

    Updates to db

--- kona-swim-hub/settings.py
+++ kona-swim-hub/settings.py
@@ -92,6 +92,17 @@ if 'RDS_DB_NAME' in os.environ:
             'PORT': os.environ['RDS_PORT'],
         }
     }
+else :
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': os.environ['RDS_DB_NAME'],
+            'USER': os.environ['RDS_USERNAME'],
+            'PASSWORD': os.environ['RDS_PASSWORD'],
+            'HOST': os.environ['RDS_HOSTNAME'],
+            'PORT': os.environ['RDS_PORT'],
+        }
+    }
 # else:
 #    DATABASES = {"default": env.dj_db_url("DATABASE_URL")}    
 #    DATABASES = {
