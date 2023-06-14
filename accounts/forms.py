9cd874f9519b7bd2da3b4a3031926f185bc0b667
Lizzy <elizabethpine4@gmail.com>
2023-06-14 07:35:35 -1000

    Update add user form

--- accounts/forms.py
+++ accounts/forms.py
@@ -5,74 +5,78 @@ from django.forms import ModelForm
 from accounts.models import CustomUser
 from lessons.models import Learner
 
+
 class CustomUserCreationForm(UserCreationForm):
     is_instructor = forms.BooleanField(
-        label=('Check if would you like to register as a swim instructor.'),
+        label=("Check if would you like to register as a swim instructor."),
     )
 
     class Meta:
         model = CustomUser
-        fields = UserCreationForm.Meta.fields + ('first_name', 'last_name', 'email', 'is_instructor',)
-    
+        fields = UserCreationForm.Meta.fields + (
+            "first_name",
+            "last_name",
+            "email",
+            "is_instructor",
+        )
+
+
 class CustomUserChangeForm(UserChangeForm):
     class Meta:
         model = CustomUser
         fields = UserChangeForm.Meta.fields
 
+
 class LearnerAddForm(ModelForm):
     class Meta:
         model = Learner
-        fields = ['birthday', 'first_name', 'last_name']
-        exclude = ['enrolled_in_course', 'learner_on_waitlist', 'associated_with_user']
-        widgets = {
-        'birthday': DatePickerInput()
-        }
+        fields = ["birthday", "first_name", "last_name"]
+        exclude = ["enrolled_in_course", "learner_on_waitlist", "associated_with_user"]
+        widgets = {"birthday": DatePickerInput()}
+
 
 class ContactForm(forms.Form):
     from_email = forms.EmailField(max_length=50, required=True)
     subject = forms.CharField(max_length=100, required=True)
-    message = forms.CharField(widget = forms.Textarea, max_length=2000, required=True)
+    message = forms.CharField(widget=forms.Textarea, max_length=2000, required=True)
+
 
 class ContactInstructorForm(forms.Form):
     subject = forms.CharField(max_length=100, required=True)
-    message = forms.CharField(widget = forms.Textarea, max_length=2000, required=True)
+    message = forms.CharField(widget=forms.Textarea, max_length=2000, required=True)
+
 
 class ContactLearnersForm(forms.Form):
-
     def __init__(self, *args, **kwargs):
-        course = kwargs.pop('course')
+        course = kwargs.pop("course")
         super(ContactLearnersForm, self).__init__(*args, **kwargs)
-        self.fields['recipient_list'].queryset = Learner.objects.filter(learners=course)
+        self.fields["recipient_list"].queryset = Learner.objects.filter(learners=course)
 
     recipient_list = forms.ModelMultipleChoiceField(
-        label=('Who would you like to receive this message?'),
+        label=("Who would you like to receive this message?"),
         queryset=None,
         widget=forms.CheckboxSelectMultiple,
         required=True,
-        error_messages = {
-            'required':'You must select a recipient.'
-            }
-        )
-
+        error_messages={"required": "You must select a recipient."},
+    )
     subject = forms.CharField(max_length=100, required=True)
-    message = forms.CharField(widget = forms.Textarea, max_length=2000, required=True)
+    message = forms.CharField(widget=forms.Textarea, max_length=2000, required=True)
+
 
 class ContactWaitlistForm(forms.Form):
-
     def __init__(self, *args, **kwargs):
-        course = kwargs.pop('course')
+        course = kwargs.pop("course")
         super(ContactWaitlistForm, self).__init__(*args, **kwargs)
-        self.fields['recipient_list'].queryset = Learner.objects.filter(waitlisted=course)
+        self.fields["recipient_list"].queryset = Learner.objects.filter(
+            waitlisted=course
+        )
 
     recipient_list = forms.ModelMultipleChoiceField(
-        label=('Who would you like to receive this message?'),
+        label=("Who would you like to receive this message?"),
         queryset=None,
         widget=forms.CheckboxSelectMultiple,
         required=True,
-        error_messages = {
-            'required':'You must select a recipient.'
-            }
-        )
-
+        error_messages={"required": "You must select a recipient."},
+    )
     subject = forms.CharField(max_length=100, required=True)
-    message = forms.CharField(widget = forms.Textarea, max_length=2000, required=True)
+    message = forms.CharField(widget=forms.Textarea, max_length=2000, required=True)
--- accounts/models.py
+++ accounts/models.py
@@ -3,26 +3,36 @@ from django.db import models
 
 # Create your models here.
 
+
 class CustomUser(AbstractUser):
     email = models.EmailField(max_length=200)
     password = models.CharField(max_length=200)
-    first_name = models.CharField(max_length = 100)
-    last_name = models.CharField(max_length = 100)
-    is_instructor = models.BooleanField(default=False)
-    course_created = models.ForeignKey('lessons.Course', null=True, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    is_instructor = models.BooleanField(default=False, blank=True)
+    course_created = models.ForeignKey(
+        "lessons.Course", null=True, on_delete=models.CASCADE
+    )
+
     def __str__(self):
         return self.first_name[:50] + " " + self.last_name[:50]
 
+
 class Learner(models.Model):
-    associated_with_user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, default=None, blank=True, null=True)
-    first_name = models.CharField(max_length = 100)
-    last_name = models.CharField(max_length = 100)
+    associated_with_user = models.ForeignKey(
+        CustomUser, on_delete=models.CASCADE, default=None, blank=True, null=True
+    )
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
     birthday = models.DateField()
+
     def __str__(self):
         return self.first_name[:50] + " " + self.last_name[:50]
-    
+
+
 class Profile(models.Model):
     user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
     bio = models.TextField()
+
     def __str__(self):
         return self.customuser.email
