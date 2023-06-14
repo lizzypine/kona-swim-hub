e48f20feccf0cc5608ebcf3c41294471c6a9c891
Lizzy <elizabethpine4@gmail.com>
2023-06-14 07:24:53 -1000

    Update database settings

--- kona-swim-hub/settings.py
+++ kona-swim-hub/settings.py
@@ -85,16 +85,16 @@ WSGI_APPLICATION = "kona-swim-hub.wsgi.application"
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 
-# if 'RDS_DB_NAME' in os.environ:
-if "RDS_HOSTNAME" in os.environ:
+if "RDS_DB_NAME" in os.environ:
+    # if "RDS_HOSTNAME" in os.environ:
     DATABASES = {
         "default": {
+            "ENGINE": "django.db.backends.postgresql_psycopg2",
             "NAME": os.environ["RDS_DB_NAME"],
             "USER": os.environ["RDS_USERNAME"],
             "PASSWORD": os.environ["RDS_PASSWORD"],
             "HOST": os.environ["RDS_HOSTNAME"],
             "PORT": os.environ["RDS_PORT"],
-            "ENGINE": "django.db.backends.postgresql_psycopg2",
         }
     }
 
@@ -138,59 +138,15 @@ USE_I18N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-# STATIC_URL = '/static/'
-# STATIC_ROOT = 'static'
-
-# STATIC_URL = "/static/"
-# STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
-# STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-
-# STATIC_ROOT = os.path.join(BASE_DIR, '..', 'static')
-
-# STATIC_URL = '/static/'
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, 'static'),
-# ]
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
-
 STATIC_URL = "/static/"
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
-# STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATIC_ROOT = os.path.join(BASE_DIR, "..", "www", "static")
 
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-# STATIC_ROOT = 'static'
-
-# STATIC_URL = '/staticfiles/'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-USE_S3 = os.getenv("USE_S3") == "TRUE"
-# SECRET_KEY = os.environ['SECRET_KEY']
-# SECRET_KEY = os.getenv['SECRET_KEY']
-
-# if USE_S3:
-#     # aws settings
-#     # AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
-#     AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
-# SECRET_KEY = os.environ['SECRET_KEY']
-#     # SECRET_KEY = os.getenv['SECRET_KEY']
-#     # AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
-#     # AWS_S3_BUCKET_NAME_STATIC = AWS_STORAGE_BUCKET_NAME
-#     AWS_DEFAULT_ACL = 'public-read'
-#     # AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
-#     AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
-#     # AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
-#     # STATIC_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
-#     AWS_LOCATION = 'static'
-#     # STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_LOCATION}/'
-#     # STATICFILES_STORAGE = 'django_s3_storage.storage.StaticS3Storage'
-# else:
-# STATIC_URL = '/staticfiles/'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# Remove?
+# USE_S3 = os.getenv("USE_S3") == "TRUE"
 
 if "AWS_STORAGE_BUCKET_NAME" in os.environ:
     STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
@@ -201,14 +157,6 @@ if "AWS_STORAGE_BUCKET_NAME" in os.environ:
 
     AWS_S3_ACCESS_KEY_ID = os.environ["AWS_ACCESS_KEY_ID"]
     AWS_S3_SECRET_ACCESS_KEY = os.environ["AWS_SECRET_ACCESS_KEY"]
-
-
-# if DEBUG:
-#     STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, 'static'),
-#     ]
-# else:
-#     STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
