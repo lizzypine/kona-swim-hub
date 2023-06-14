ab8d28740a5b4c6147a35ce0998389bdca8d1182
Lizzy <elizabethpine4@gmail.com>
2023-06-13 22:28:33 -1000

    Update handling of static files

--- kona-swim-hub/settings.py
+++ kona-swim-hub/settings.py
@@ -87,7 +87,6 @@ if 'RDS_HOSTNAME' in os.environ:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            # 'ENGINE': 'django.db.backends.postgresql',
             'NAME': os.environ['RDS_DB_NAME'],
             'USER': os.environ['RDS_USERNAME'],
             'PASSWORD': os.environ['RDS_PASSWORD'],
