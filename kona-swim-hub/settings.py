f45f41499704d2df054552791621638658c4a84a
Lizzy <elizabethpine4@gmail.com>
2023-06-13 14:46:10 -1000

    Installed psycopg2

--- kona-swim-hub/settings.py
+++ kona-swim-hub/settings.py
@@ -81,7 +81,9 @@ WSGI_APPLICATION = 'kona-swim-hub.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-if 'RDS_DB_NAME' in os.environ:
+
+# if 'RDS_DB_NAME' in os.environ:
+if 'RDS_HOSTNAME' in os.environ:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql_psycopg2',
